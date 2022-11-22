d9ce7ec6608dbeaa110a944e2894da32bce26098
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-22 22:12:30 +0100

    integrate all the front end application routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/presentation', function () {
+    return 'page de presentation';
+});
+
+Route::get('/services', function () {
+    return 'page du services';
+});
+
+Route::get('/service-a-domicile', function () {
+    return 'page service a domicile';
+});
+
+Route::get('/nos-avantages', function () {
+    return 'page de nos avantages';
+});
+
+Route::get('/contact', function () {
+    return 'page de contact';
+});
+
+Route::get('/mentions-legales', function () {
+    return 'page de mentions legales';
+});
