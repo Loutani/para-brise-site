1ec0324b7ceab797b533ac2966e6b8be96719b58
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-22 22:31:37 +0100

    integrate the header content

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,12 @@
-<p>HEAEDR</p>
+<header>
+    <h1>LES PROS QUI TOMBENT A PIC</h1>
+
+    <h3>Une fissure ou un impact ?</h3>
+
+    <p>Nous sommes les spécialistes rapides et amicaux qui peuvent réparer tous les types de dommages causés par les débris de la route ou les éclats de pierre. Grâce à nos garnitures de qualité supérieure, assorties d’une garantie de main-d’œuvre à vie, vous pouvez vous détendre en sachant que votre voiture est entre de bonnes mains.</p>
+
+    <button type="button">
+        {{-- todo:: add phone icon here by using font awesome --}}
+        contactez-nous
+    </button>
+</header>
