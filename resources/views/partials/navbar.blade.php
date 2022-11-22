c01c682216681253a5336e69ba0674a84e4cc49c
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-22 22:27:28 +0100

    integrate the navbar content

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,1 +1,24 @@
-<p>NAVBAR</p>
+<nav class="navbar">
+    <div class="navbar-logo">
+        <img src="navbar-logo-image" alt="">
+    </div>
+    <div class="navbar-items">
+        <ul>
+            <li>
+                <a href="{{route('home')}}">Accueil</a>
+            </li>
+            <li>
+                <a href="{{route('presentation')}}">Présentation</a>
+            </li>
+            <li>
+                <a href="{{route('service.index')}}">Services</a>
+            </li>
+            <li>
+                <a href="{{route('advantages')}}">Les avantages L’as De Carreau</a>
+            </li>
+            <li>
+                <a href="{{route('contact')}}">PRENDRE RENDEZ-VOUS</a>
+            </li>
+        </ul>
+    </div>
+</nav>
