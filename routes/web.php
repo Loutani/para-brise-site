00ae2407e4703d7fe55afeec6b79c8f3f581f628
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-22 22:18:59 +0100

    give name for routes

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/presentation', function () {
     return 'page de presentation';
-});
+})->name('presentation');
 
 Route::get('/services', function () {
     return 'page du services';
-});
+})->name('service.index');
 
 Route::get('/service-a-domicile', function () {
     return 'page service a domicile';
-});
+})->name('service.domicile');
 
 Route::get('/nos-avantages', function () {
     return 'page de nos avantages';
-});
+})->name('advantages');
 
 Route::get('/contact', function () {
     return 'page de contact';
-});
+})->name('contact');
 
 Route::get('/mentions-legales', function () {
     return 'page de mentions legales';
-});
+})->name('legal.mentions');
